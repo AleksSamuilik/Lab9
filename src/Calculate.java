a86f7bbb1c51a220ca7e67ef9d543c6f9a9b1cc2
Spawn <alkspawn@gmail.com>
2019-03-26 21:03:09 +0300

    Normal calc

--- src/Calculate.java
+++ src/Calculate.java
@@ -7,11 +7,11 @@ public class Calculate {
         Scanner scanner = new Scanner(System.in);
         boolean checkInput = true;
         while (checkInput) {
-          String   number = scanner.nextLine();
+            String number = scanner.nextLine();
             try {
                 long xxx = Long.parseLong(number);
                 if (xxx >= 0) {
-                    checkInput=false;
+                    checkInput = false;
                     return xxx;
                 } else
                     System.out.println("Sorry. Try again!");
@@ -23,15 +23,52 @@ public class Calculate {
         return -1;
     }
 
+    public static int readOperation() {
+        Scanner scanner = new Scanner(System.in);
+        boolean checkInput = true;
+        while (checkInput) {
+            String number = scanner.nextLine();
+            try {
+                String add = "+";
+                String substract = "-";
+                String multiply = "*";
+                if (number.equals(add)) {
+                    return 0;
+                } else if (number.equals(substract)) {
+                    return 1;
+                } else if (number.equals(multiply)) {
+                    return 2;
+                } else
+                    System.out.println("Sorry. Try again!");
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry. Try again!");
+            }
+        }
+        scanner.close();
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Write the first number:");
         long firstNumber = readUserInput();
         System.out.println("Write the second number:");
-        long secondNumber=readUserInput();
-        System.out.println("Your the first number: "+firstNumber + "\nYour the second number: "+secondNumber);
-        System.out.println("What operation do you want to do?");
-
-
-
+        long secondNumber = readUserInput();
+        System.out.println("Your the first number: " + firstNumber + "\nYour the second number: " + secondNumber);
+        System.out.println("What operation do you want to do? ( '   +   '   -   '   *   ')");
+        int check = readOperation();
+        switch (check) {
+            case 0:
+                long resultAdd = firstNumber + secondNumber;
+                System.out.println("Result: " + resultAdd);
+                break;
+            case 1:
+                long resultSubstract = firstNumber - secondNumber;
+                System.out.println("Result: " + resultSubstract);
+                break;
+            case 2:
+                long resultMultiply = firstNumber * secondNumber;
+                System.out.println("Result: " + resultMultiply);
+        }
     }
 }
